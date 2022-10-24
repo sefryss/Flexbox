229f8377d37b8102d427a4111b150d01041f0268
sefryss <109064862+sefryss@users.noreply.github.com>
2022-10-24 15:58:23 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View
+        style = {{
+          flex: 1
+        }}
+      >
+       <View 
+        style = {{
+          alignItems: 'center',
+          backgroundColor: 'lightgrey',
+          flex: 1,
+          justifyContent: 'center'
+        }}
+        >
+
+        <Text> 
+          Kosong masih belum ada item
+        </Text>
+      </View>
+
+        <View
+          style = {{
+            backgroundColor: 'gray',
+            flexDirection : 'row',
+            justifyContent: 'space-between',
+            padding : 10
+          }}
+        >
+          <View
+            style = {{
+              backgroundColor: 'yellow',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+
+          <View
+            style = {{
+              backgroundColor: 'white',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+
+          <View
+            style = {{
+              backgroundColor: 'white',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
